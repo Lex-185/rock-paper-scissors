93439cd1c33bf31ad498b56cb17178a101e405c1
Lex <gravityandmadness@gmail.com>
2022-08-08 09:54:17 +0200

    Added function to play a single round

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
+// Play 1 round
+function playRound(p, c) {
+    if (p === c) {
+        return `It's a draw. Player choice: ${p}. Computer choice: ${c}`
+    } else if ((p === 'rock' && c === 'scissors') 
+    || (p === 'scissors' && c === 'paper') 
+    || (p === 'paper' && c === 'rock')) {
+        scoreboard.player++
+        return `You win! ${p} beats ${c}`
+    } else if ((p === 'scissors' && c === 'rock') 
+    || (p === 'paper' && c === 'scissors') 
+    || (p === 'rock' && c === 'paper')) {
+        scoreboard.computer++
+        return  `You lose! ${c} beats ${p}`
+    }
+}
+
 
 
 
